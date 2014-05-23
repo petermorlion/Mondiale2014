4c1165e84254ebd3e32172f6df38a10b2af84165
Peter Morlion <peter.morlion@live.be>
2014-05-23 13:15:21 +0200

    added angular, only load in Index page

--- KotProno2/App_Start/BundleConfig.cs
+++ KotProno2/App_Start/BundleConfig.cs
@@ -23,6 +23,12 @@ namespace KotProno2
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-animate.js",
+                      "~/Scripts/angular-route.js",
+                      "~/Scripts/angular-sanitize.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
