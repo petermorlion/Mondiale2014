3a7a50a48004c4377e2a0c515f82f398ef5ff64e
petermorlion <peter.morlion@live.be>
2014-06-06 18:22:49 +0200

    got it to work on azure, removed elmah

--- KotProno2/Controllers/MatchesController.cs
+++ KotProno2/Controllers/MatchesController.cs
@@ -5,6 +5,7 @@ using KotProno2.Models;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
