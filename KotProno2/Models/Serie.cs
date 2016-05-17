98487ec3a98018dd138bad6116002442ff0b3621
Peter Morlion <peter.morlion@live.be>
2016-05-17 12:23:00 +0200

    Added some info + cleanup

--- KotProno2/Models/Serie.cs
+++ KotProno2/Models/Serie.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace KotProno2.Models
 {
+    /// <summary>
+    /// Represents a person's score as plotted on a chart.
+    /// </summary>
     public class Serie
     {
         public string Name { get; set; }
