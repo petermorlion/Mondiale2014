2e4af14497ab951edcd02ed9b299232183a33c36
Peter Morlion <peter.morlion@live.be>
2016-05-17 12:26:49 +0200

    Added Tournament class

---
+++ KotProno2/Models/Tournament.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KotProno2.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Match> Matches { get; set; }
+    }
+}
