295eada98987b179c40d405a99b04cbc8fdf10ac
Peter Morlion <peter.morlion@live.be>
2016-05-19 19:55:12 +0200

    Angular now loads navigation

---
+++ KotProno2/Controllers/NavController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace KotProno2.Controllers
+{
+    public class NavController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
