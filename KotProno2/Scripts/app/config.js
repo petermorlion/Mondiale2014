bda9b6b7a8b9457dd73f79b20ff439d59945ac0a
Peter Morlion <peter.morlion@live.be>
2016-05-27 20:51:05 +0200

    cleanup of unused code

--- KotProno2/Scripts/app/config.js
+++ KotProno2/Scripts/app/config.js
@@ -3,11 +3,4 @@
 
     var app = angular.module('app');
 
-    // For use with the HotTowel-Angular-Breeze add-on that uses Breeze
-    var remoteServiceName = 'breeze/matches';
-    
-    var config = {
-    };
-
-    app.value('config', config);
 })();
