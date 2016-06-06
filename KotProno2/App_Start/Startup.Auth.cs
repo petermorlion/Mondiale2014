0c804217e901ea5f60c80a3e9e56b7ca90d5b991
Peter Morlion <peter.morlion@live.be>
2016-06-06 10:44:34 +0200

    redirect when unauthorized

--- KotProno2/App_Start/Startup.Auth.cs
+++ KotProno2/App_Start/Startup.Auth.cs
@@ -15,7 +15,7 @@ namespace KotProno2
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
+                LoginPath = new PathString("/#/login")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
