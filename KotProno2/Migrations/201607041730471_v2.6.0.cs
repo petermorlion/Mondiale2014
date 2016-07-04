51ca663dd665dc13056d428f56f30da3490554a2
Peter Morlion <peter.morlion@gmail.com>
2016-07-04 19:43:36 +0200

    Add euro 2016 semi-finals

---
+++ KotProno2/Migrations/201607041730471_v2.6.0.cs
@@ -0,0 +1,24 @@
+namespace KotProno2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v260 : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("DECLARE @TournamentId INT;" +
+                "SET @TournamentId = (SELECT Id FROM dbo.Tournaments WHERE Name = 'EK 2016');" +
+                "INSERT INTO dbo.Matches (HomeTeamIsoCode, AwayTeamIsoCode, DateTime, Stage, IsReadOnly, Tournament_Id)" +
+                "VALUES ('pt', '_Wales', '2016/07/06 21:00', 3, 0, @TournamentId)," +
+                "       ('de', 'fr', '2016/07/07 21:00', 3, 0, @TournamentId)" +
+                ";"
+                );
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM dbo.Matches WHERE DateTime > '2016/06/30'");
+        }
+    }
+}
