b20bb9f111d539c4a4eb35ebfb30e650d79c1a91
Peter Morlion <peter.morlion@gmail.com>
2018-07-03 15:52:57 +0200

    Bump version in template cache buster

--- KotProno2/Scripts/app/preventTemplateCache.js
+++ KotProno2/Scripts/app/preventTemplateCache.js
@@ -2,10 +2,10 @@
     'use strict';
     angular.module('app')
         .factory('preventTemplateCache', function ($injector) {
-            var build = '3.2.0';
+            var build = '3.2.1';
             return {
                 'request': function (config) {
-                    if (config.url.indexOf('Scripts/app/') !== -1) {
+                    if (config.url.indexOf('Scripts/app/') !== -1 && config.url.indexOf('.html') !== -1) {
                         config.url = config.url + '?t=' + build;
                     }
                     return config;
