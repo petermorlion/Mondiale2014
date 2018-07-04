b3192a08793ca17aed16a5f11fe4ccb1ab5a9c2d
Peter Morlion <peter.morlion@gmail.com>
2018-07-04 09:08:58 +0200

    Add header comment to generated css file

--- KotProno2/gulpfile.js
+++ KotProno2/gulpfile.js
@@ -2,12 +2,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var headerComment = require('gulp-header-comment');
 
 gulp.task('less', function () {
     return gulp.src('./Content/Site.less')
       .pipe(less({
           paths: [path.join(__dirname, 'less', 'includes')]
-      }))
+        }))
+      .pipe(headerComment('This file was generated. Edit the source file for any changes or they will be overwritten.'))
       .pipe(gulp.dest('./Content/'));
 });
 
