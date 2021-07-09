cc3f8121436a83a0f1fb34af6862f1598405c432
Peter Morlion <peter.morlion@gmail.com>
2021-07-09 08:21:42 +0200

    Add finals

---
+++ KotProno2/Migrations/202107090607112_v3.11.0.cs
@@ -0,0 +1,22 @@
+﻿namespace KotProno2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v3110 : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("DECLARE @TournamentId INT;" +
+                   "SET @TournamentId = (SELECT Id FROM dbo.Tournaments WHERE Name = 'Euro 2020');" +
+                   "INSERT INTO dbo.Matches (HomeTeamIsoCode, AwayTeamIsoCode, DateTime, Stage, Tournament_Id)" +
+                   "VALUES ('it', '_England', '2021/07/11 21:00', 4, @TournamentId)" +
+                   ";");
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM dbo.Matches WHERE DateTime > '2021/07/10'");
+        }
+    }
+}
